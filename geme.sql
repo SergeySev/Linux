ad7c88a06ec4040d9b8a9fc6d1ea69c34d9cb532
SergeySev <89078394+SergeySev@users.noreply.github.com>
2023-02-07 09:55:03 +0100

    create sql game tables

---
+++ geme.sql
@@ -0,0 +1,30 @@
+create table items (
+    id int auto_increment primary key,
+    cost int,
+    description varchar(150),
+    add_date date
+);
+
+create table tranzaktion (
+    id int auto_increment primary key,
+    id_player int,
+    sum int,
+    id_item int,
+    buy_date date,
+    quantity int,
+    FOREIGN KEY (id_item) REFERENCES items(id)
+);
+
+create table players (
+    user_id int auto_increment primary key,
+    email varchar(250),
+    name varchar(250),
+    password varchar(16),
+    number_of_points int,
+    free_game_cash int,
+    donate_game_cash int,
+    isBanned boolean,
+    id_tranzaktion int,
+    FOREIGN KEY (id_tranzaktion) REFERENCES tranzaktion(id)
+);
+
